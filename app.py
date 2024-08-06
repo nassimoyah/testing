3189fbe65a82ab3a1afff705ac36ebf496a261f6
nassimoyah <nassimzam5@gmail.com>
2024-08-06 17:29:39 +0100

    nrl last

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask ,Response ,request,redirect,url_for,render_template ,jsonify
 from pymongo import MongoClient
 import json
-from bson.objectid import ObjectId   # we can use the object id as text 
+
 
 
 app = Flask(__name__)    #creating wsgi application "web standared gataway interface"
